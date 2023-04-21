fb5a45036237523e4d537116b907b19e6b602c69
Jonjo7Dia <jonosphgames@gmail.com>
2023-04-21 15:09:15 +0200

    Set up slice machine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Link from "next/link";
+import { PrismicProvider } from "@prismicio/react";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "../prismicio";
+
+interface AppProps {
+  Component: React.ComponentType<any>;
+  pageProps: any;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  );
 }
---
+++ slices/slices.d.ts
@@ -0,0 +1,4 @@
+declare module "../slices" {
+  export const components: any; // Replace 'any' with the actual type of components
+  // Add more exports here if necessary
+}
