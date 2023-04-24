1f47d0222673369113b654ef5d14ea65fe446627
Jonjo7Dia <jonosphgames@gmail.com>
2023-04-24 13:48:23 +0200

    Make hero images priority

--- components/HeroBlock.tsx
+++ components/HeroBlock.tsx
@@ -19,6 +19,7 @@ export default function HeroBlock(props: HeroProps) {
           className={classes["hero-block__background__image"]}
           width={1204}
           height={800}
+          priority
         />
       </div>
       <div className={classes["hero-block__content"]}>
